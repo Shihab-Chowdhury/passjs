c65b769e7fcb10a0a7b36639311c0740e0aa9540
Shihab <cshihab04@gmail.com>
2022-08-07 17:30:50 +0600

    Initial Commit

--- app.js
+++ app.js
@@ -3,22 +3,20 @@ const express = require('express')
 const app = express();
 
 function middleware1(req, res, next) {
-    console.log('I am a middleware1');
+    req.custom = 100;
     next();    
 }
 
 function middleware2(req, res, next) {
-    console.log('I am a middleware2');
-    const errObj = "<h1>I am an error</h1>"
-
-    next(errObj);    
+    console.log(`Custom: ${req.custom}`);
+    req.custom = 200
+    next();    
 }
 
 function errorHandler(err, req, res, next) {
-    if (err) {
-        res.send('<h1>There was an error, please try again</h1>')
-    }
-    
+    res.json({ 
+        err:err
+    })    
 }
 
 app.use(middleware1)
@@ -26,7 +24,7 @@ app.use(middleware2)
 app.use(errorHandler)
 
 app.get('/', (req, res, next) =>{
-    res.send('<h1>Hello World</h1>')
+    res.send(`no it is: ${req.custom}`)
 })
 
 app.listen(3000);
